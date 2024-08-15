b83b558fbc136ec8635df4d7c5db463f851eae3e
Peter Schilling <peter@tanookilabs.com>
2024-08-15 08:31:05 -0700

    prefab: prettier (#8)

--- app/frontend/root/router.tsx
+++ app/frontend/root/router.tsx
@@ -11,7 +11,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: rootPath({}),
-        element: <LandingScreen />
+        element: <LandingScreen />,
       },
     ],
   },
