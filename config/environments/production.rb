77e1a73b97f54386ca3cee50d4ef9bd43484eec2
Peter Schilling <peter@tanookilabs.com>
2024-09-10 12:27:50 -0700

    prefab: Cache static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,11 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = {
+    "Access-Control-Allow-Origin" => "*",
+    "Cache-Control" => "public, max-age=31536000"
+  }
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
