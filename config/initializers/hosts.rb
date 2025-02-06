84424df8bfda97d056db543859be1f982f384e02
Peter Schilling <peter@tanookilabs.com>
2025-02-06 11:56:26 -0800

    prefab: Allow all dev.tanookiapp.com subdomains as a host

--- config/initializers/hosts.rb
+++ config/initializers/hosts.rb
@@ -1,4 +1,5 @@
 if Rails.env.development?
   origin_uri = URI.parse(ENV.fetch("ORIGIN"))
   Rails.application.config.hosts << origin_uri.host
+  Rails.application.config.hosts << /.*\.dev.tanookiapp\.com/
 end
