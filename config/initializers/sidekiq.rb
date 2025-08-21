186e48ac87ae6e4ad3a323b2b45d7e0ad69c84a6
Peter Schilling <peter@tanookilabs.com>
2025-08-21 13:56:08 -0700

    prefab: Log sidekiq in development

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,17 @@
 Sidekiq.strict_args!
 
 Sidekiq.configure_server do |config|
-  config.redis = {url: ENV[ENV.fetch("REDIS_PROVIDER", "REDIS_URL")] || Rails.application.credentials.dig(:redis, :url) || "redis://localhost:6379/1"}
+  config.redis = {
+    url: ENV[ENV.fetch("REDIS_PROVIDER", "REDIS_URL")] ||
+      Rails.application.credentials.dig(:redis, :url) ||
+      "redis://localhost:6379/1"
+  }
 
-  if Rails.env.development?
-    config.logger.level = Logger::WARN
-  end
-
-  config.death_handlers << lambda do |worker, ex|
-    Rails.logger.error "Worker #{worker} failed with exception: #{ex.message}"
-    Sentry.capture_exception(ex, {data: worker.to_s})
-  end
+  config.death_handlers <<
+    lambda do |worker, ex|
+      Rails.logger.error("Worker #{worker} failed with exception: #{ex.message}")
+      Sentry.capture_exception(ex, {data: worker.to_s})
+    end
 
   # Middleware server attachment
   # config.client_middleware do |chain|
@@ -20,14 +21,17 @@ Sidekiq.configure_server do |config|
   #   chain.add SidekiqMiddleware::Server::ActorSetter
   # end
 
-  config.on :startup do
+  config.on(:startup) do
     ActiveRecord::Base.connection_handler.clear_active_connections!
   end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {url: ENV[ENV.fetch("REDIS_PROVIDER", "REDIS_URL")] || Rails.application.credentials.dig(:redis, :url) || "redis://localhost:6379/1"}
-
+  config.redis = {
+    url: ENV[ENV.fetch("REDIS_PROVIDER", "REDIS_URL")] ||
+      Rails.application.credentials.dig(:redis, :url) ||
+      "redis://localhost:6379/1"
+  }
   # Middleware Client attachment
   # config.client_middleware do |chain|
   #   chain.add SidekiqMiddleware::Client::ActorJobAttribute
